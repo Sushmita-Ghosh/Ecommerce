b256987367615f2cc65577e0209a85c57b28d60f
Sushmita Ghosh <pujaghosh251096@gmail.com>
2021-05-09 22:05:28 +0530

    updating our shop page, added the components collections item and preview

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import React from "react";
-import HomePage from "./components/pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
       </div>
     );
   }
