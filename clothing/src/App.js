3ca612da017d8f316259d432b31ab5c73d9f6449
Sushmita Ghosh <pujaghosh251096@gmail.com>
2021-05-12 11:00:34 +0530

    updating our app to redirect to home page in case the user is signed in

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
@@ -50,16 +50,33 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStatetoProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 // we do not need mapStatetoProps , since we are not using the this.state , hence null is the first parameter
-export default connect(null, mapDispatchToProps)(App);
+// but we need the current user to redirect so in next step we will use the mapStatetoProps
+// export default connect(null, mapDispatchToProps)(App);
+
+export default connect(mapStatetoProps, mapDispatchToProps)(App);
--- clothing/src/redux/user/user.actions.js
+++ clothing/src/redux/user/user.actions.js
@@ -1,7 +1,8 @@
 // The js file is for structing our actions(like user clicks etc) into proper redux actionss
 // which contain type and payload
 
+import { UserActionTypes } from "./user.types";
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
